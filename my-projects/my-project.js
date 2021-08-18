e35c1c2cc1526feb66c6c46580507890655f92a8
nmetens <metens@pdx.edu>
2021-08-17 17:38:05 -0700

    "Adding my-project page files"

---
+++ my-projects/my-project.js
@@ -0,0 +1,23 @@
+
+// Load the navbar 
+$(document).ready(() => {
+    $(() => {
+        $(".navigation").load("../navbar/navbar.html");
+    });
+});
+
+let pendulum_link = document.querySelector(".card1");
+let code_game = document.querySelector(".card2");
+let java_parser = document.querySelector(".card3");
+
+pendulum_link.addEventListener("click", () => {
+    window.location = "https://github.com/nmetens/The-Pendulum-Wave-Machine";
+});
+
+code_game.addEventListener("click", () => {
+    window.location = "https://github.com/nmetens/The-Pendulum-Wave-Machine";
+});
+
+java_parser.addEventListener("click", () => {
+    window.location = "https://github.com/nmetens/The-Pendulum-Wave-Machine";
+});
