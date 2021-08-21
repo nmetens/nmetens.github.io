d4d27d5277c7dd5d4c6bc7bda5e53788d279e06e
nmetens <metens@pdx.edu>
2021-08-21 01:16:27 -0700

    Recreated all aspects of the page in Jquery instead of HTML

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,1 +1,28 @@
 
+$(document).ready(() => {
+    $("#title").append(
+        $("<h1>").text("Nathan")
+        .css({
+            marginLeft: "10%",
+            color: "black",
+            fontFamily: "Arial",
+            fontSize: "12rem"
+        }),
+        $("<h1>").text("Metens")
+        .css({
+            marginLeft: "40%",
+            color: "black",
+            fontFamily: "Arial",
+            fontSize: "12rem"
+        })
+    ),
+    $("#copyright").append(
+        $("<div>").text("\u00A9 2021 nmetens, Inc.")
+        .css({
+            textAlign: "center",
+            color: "whitesmoke",
+            position: "relative",
+            marginTop: "40vh"
+        })
+    );
+});
