8443268f7927d6cab8110ea17287d090cea7d594
nmetens <metens@pdx.edu>
2021-08-24 22:06:57 -0700

    Added comments

--- about-me/about.js
+++ about-me/about.js
@@ -1,9 +1,10 @@
 
 $(document).ready(() => {
+    // Load the navbar:
     $(() => {
         $(".navbar").load("../navbar/navbar.html");
     });
-    $("#copyright").append(
+    $("#copyright").append( // Create copyright
         $("<div>").text("\u00A9 2021 nmetens")
         .css({
             textAlign: "center",
@@ -14,34 +15,46 @@ $(document).ready(() => {
     );
 });
 
-function showSlides(n) {
-    var slides = document.getElementsByClassName("slides");
-    if (n > slides.length) {
-        slideIndex = 1;
+// Create slideshow function:
+function slideShow(n) {
+    var slides = document.getElementsByClassName("slides"); // Get slides class
+    console.log("Slide ", slideIndex + "/" + slides.length);
+    if (n > slides.length) { // Slide.length = 8 slides
+        slideIndex = 1; // If the slide index passes 8, then go back to slide #1
     }    
     if (n < 1) {
-        slideIndex = slides.length;
+        slideIndex = slides.length; // If the slide index gows below slide #1, go to slide #8
     }
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";   // Hide all of the slides
     }
-    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block"; // Show the slide we are currently on
+
+    // let num = document.createElement("div");
+    // num.textContent = slideIndex;
+    // console.log(num)
+
+    // slides[slideIndex-1].appendChild(num + "/" + slides.length);
+    // slides[slideIndex-1].removeChild(num);
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1; // The slide we are on (start on slide #1)
+slideShow(slideIndex); // Pass slide #1 to slideshow function
     
-const plusSlides = (n) => {
-    showSlides(slideIndex += n);
+const plusSlides = (plus_or_minus) => { // To go to the next slide, incriment or decrement the slideIndex always by a value of +- 1
+    slideShow(slideIndex += plus_or_minus);
 }
 
+// Create variables for the next and prev buttons:
 let prev_btn = document.getElementById("prev");
 let next_btn = document.getElementById("next");
 
+// Decrement the slide number when clicking on the prev button:
 prev_btn.onclick = (event) => {
     plusSlides(-1);
 };
 
+// Increment the slide number when clicking on the prev button:
 next_btn.onclick = (event) => {
     plusSlides(1);
 };
