25c3f017f4b1eeb5b93f9e09c2b8b0250bbe1892
nmetens <metens@pdx.edu>
2021-08-24 22:08:06 -0700

    Added comments

--- contact/contact.js
+++ contact/contact.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
             $(".navbar").load("../navbar/navbar.html");
         });
     });
-    $("#copyright").append(
+    $("#copyright").append( // Create the copyright in the footer element
         $("<div>").text("\u00A9 2021 nmetens")
         .css({
             textAlign: "center",
