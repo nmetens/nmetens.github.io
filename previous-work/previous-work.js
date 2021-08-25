3105843df288ef94e4fdcdaeca71a42646532d21
nmetens <metens@pdx.edu>
2021-08-24 22:07:43 -0700

    Fixed problems with image resizing on smaller and larger screen widths and added comments

--- previous-work/previous-work.js
+++ previous-work/previous-work.js
@@ -1,32 +1,31 @@
-// Load the navbar from the navbar file
 $(document).ready(() => {
-  // Load the navigation html code:
+  // Load the navbar html code:
     $(() => {
         $(".navbar").load("../navbar/navbar.html");
     });
     // CSS in JQuery for styling elements:
     $(".page-content").css({
-      width: "70%",
+      width: "80%",
       margin: "auto",
       height: "100%",
       borderRadius: "10px"
     })
-    $("h1").css({
+    $("h1").css({ // Page title
       textAlign: "center",
       fontWeight: "bold",
       color: "whitesmoke",
       textShadow: "3px 3px 3px #000, 0 0 5px #000"
     });
-    $("h3").css({
+    $("h3").css({ // Job titles/dates worked
       color: "whitesmoke",
       textAlign: "center",
     });
-    $(".work").css({
+    $(".work").css({ // Job description and details
       color: "whitesmoke",
       opacity: "none",
       padding: "15px"
     });
-    $("#copyright").append(
+    $("#copyright").append( // Copyright footer
       $("<div>").text("\u00A9 2021 nmetens")
       .css({
           textAlign: "center",
@@ -35,15 +34,15 @@ $(document).ready(() => {
           marginBottom: "30px"
       })
   );
-    // Got to GitHub profile in new window:
+    // Got to GitHub profile in new window on click:
     $("#github").on("click", () => {
       window.open("https://github.com/nmetens");
     })
-    // Got to LinkedIn profile in new window:
+    // Got to LinkedIn profile in new window on click:
     $("#linkedin").on("click", () => {
       window.open("https://www.linkedin.com/in/nathan-metens-52674521a/")
     });
-    // Go to Yelp page for Oofty Business:
+    // Go to Yelp page for Oofty Business on click:
     $("#yelp").on("click", () => {
       window.open("https://www.yelp.com/biz/oofty-waffles-and-cr%C3%A8pes-beaverton");
     });
